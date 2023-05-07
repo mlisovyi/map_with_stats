9ee5ad11879b494aaca00e576001abb9b64a89b2
Mischa <mischa.lisovyi@mgb.ch>
2023-05-07 16:46:55 +0200

    add tests

---
+++ test/test_utils.py
@@ -0,0 +1,17 @@
+import map_with_stats as mws
+import pytest
+import pandas as pd
+from map_with_stats.utils import _check_cols_in_df
+
+
+@pytest.mark.parametrize(
+    "hectare_id, x, y", [(6000_4000, 6000_00, 4000_00), (4000_6000, 4000_00, 6000_00)]
+)
+def test(hectare_id, x, y):
+    df = pd.DataFrame({"hid": [hectare_id], "dummy": ["dummy"]})
+    df = mws.hectare2xy(df, "hid")
+    # validate that new as well as original columns are in place
+    _check_cols_in_df(df, ["X", "Y", "hid", "dummy"])
+    # validate extracted values
+    assert df["X"].item() == x
+    assert df["Y"].item() == y
